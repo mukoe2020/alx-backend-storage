f525e6f7b1291fc41702af9189c9d51d98368c96
memoe <memoemukoe@gmail.com>
2024-03-21 07:11:01 +0000

    In and not out

---
+++ 0x00-MySQL_Advanced/1-country_users.sql
@@ -0,0 +1,8 @@
+-- a SQL script that creates a table users with specified requirements
+
+CREATE TABLE IF NOT EXISTS users (
+	id INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
+	email VARCHAR(255) UNIQUE NOT NULL,
+	name VARCHAR(255),
+	country ENUM('US', 'CO', 'TN') NOT NULL DEFAULT 'US'
+	);
