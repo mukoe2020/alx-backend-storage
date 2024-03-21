39037458285d564bd91f9ae8357f20bae1ffc1de
memoe <memoemukoe@gmail.com>
2024-03-21 07:20:29 +0000

    SQL script that creates a trigger that decreases the quantity of an item after adding a new order.

---
+++ 0x00-MySQL_Advanced/3-glam_rock.sql
@@ -0,0 +1,21 @@
+-- a SQL script that lists all bands with Glam rock as their main style, ranked by their longevity
+-- create a function that handles null values in the split column
+
+DROP FUNCTION IF EXISTS lifespan;
+
+DELIMITER //
+CREATE FUNCTION lifespan(formed INTEGER, split INTEGER)
+    RETURNS INTEGER DETERMINISTIC
+BEGIN
+    IF split IS NULL THEN
+        RETURN 2022 - formed;
+    ELSE
+        RETURN split - formed;
+    END IF;
+END//
+DELIMITER ;
+
+SELECT band_name, lifespan(formed, split) AS lifespan
+FROM metal_bands
+WHERE style LIKE '%Glam rock%'
+ORDER BY lifespan DESC;
---
+++ 0x00-MySQL_Advanced/4-store.sql
@@ -0,0 +1,12 @@
+-- a SQL script that creates a trigger that decreases the quantity of an item after adding a new order.
+
+DELIMITER **
+CREATE TRIGGER update_q AFTER INSERT ON orders
+FOR EACH ROW 
+BEGIN
+UPDATE items
+SET quantity = quantity - NEW.number
+WHERE items.name = NEW.item_name;
+END **
+
+DELIMITER;
